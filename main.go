7cf38ddf1aea4148c9634b904bbed364bbf67188
Andrew Watson <andrewmoorewatson@gmail.com>
2018-08-28 15:52:07 -0400

    added a handler that lists all the Gophers!

--- main.go
+++ main.go
@@ -1,5 +1,69 @@
 package main
 
+import (
+	"encoding/json"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
+// Gopher represents the furry creature we all love
+type Gopher struct {
+	Name          string `json:"name"`
+	Age           int    `json:"age"`
+	FavoriteColor string `json:"favoritecolor"`
+}
+
+// GopherData Holds A Slice of Gophers.
+type GopherData struct {
+	Gophers []Gopher `json:"gophers"`
+}
+
+var (
+	gophers GopherData
+)
+
+func init() {
+
+	gophers = GopherData{
+		[]Gopher{},
+	}
+}
+
 func main() {
 
+	defer db.Close()
+
+	r := mux.NewRouter()
+
+	r.Methods(http.MethodGet).Path("/").HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		// Return a list of all the Gophers in Memory
+
+		var err error
+		jsonOutput := []byte("")
+
+		if len(gophers.Gophers) > 0 {
+			jsonOutput, err = json.Marshal(gophers)
+			if err != nil {
+				errJSON, _ := json.Marshal(struct{ err string }{err.Error()})
+				http.Error(w, string(errJSON), http.StatusInternalServerError)
+				return
+			}
+		} else {
+			w.WriteHeader(http.StatusNoContent)
+			return
+		}
+
+		w.Header().Add("Content-type", "application/json")
+		w.Write(jsonOutput)
+	})
+
+	r.Methods(http.MethodPost).Path("/gopher").HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+
+		w.WriteHeader(http.StatusNotImplemented)
+		return
+	})
+
+	http.Handle("/", r)
+	http.ListenAndServe(":8080", nil)
 }
